997bbd91d1228ee4c8a3f227f749bd6225d3b5f2
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-12-03 20:43:02 +0545

    Profile Screen added

--- lib/src/routes/route_helper.dart
+++ lib/src/routes/route_helper.dart
@@ -5,10 +5,13 @@ import 'package:chat_app_flutter/src/data/views/auth/signin_screen/successful_sc
 import 'package:chat_app_flutter/src/data/views/auth/signin_screen/verification_screen.dart';
 import 'package:chat_app_flutter/src/data/views/auth/signup_screen/signup_profile_screen.dart';
 import 'package:chat_app_flutter/src/data/views/auth/signup_screen/signup_screen.dart';
+import 'package:chat_app_flutter/src/data/views/camera_screen/camera_screen.dart';
 import 'package:chat_app_flutter/src/data/views/contact_screen/contact_screen.dart';
 import 'package:chat_app_flutter/src/data/views/home_screen.dart/chat_screen.dart';
 import 'package:chat_app_flutter/src/data/views/home_screen.dart/edit_chat_screen.dart';
 import 'package:chat_app_flutter/src/data/views/message_screeen/message_screen.dart';
+import 'package:chat_app_flutter/src/data/views/phone_screen.dart/phone_screen.dart';
+import 'package:chat_app_flutter/src/data/views/profile_screen.dart/profile_screen.dart';
 import 'package:chat_app_flutter/src/data/views/splash_screen/splash_screen.dart';
 
 class RouteHelper {
@@ -23,6 +26,9 @@ class RouteHelper {
   static const String _editChatsScreen = "/edit-chats";
   static const String _contactScreen = "/contacts";
   static const String _messageScreen = "/messages";
+  static const String _phoneScreen = "/phone";
+  static const String _cameraScreen = "/camera";
+  static const String _profileScreen = "/profile";
 
   static String getInitial() => _initial;
   static String getAuthScreen() => _authScreen;
@@ -35,6 +41,9 @@ class RouteHelper {
   static String getEditChatScreen() => _editChatsScreen;
   static String getContactScreen() => _contactScreen;
   static String getMessageScreen() => _messageScreen;
+  static String getPhoneScreen() => _phoneScreen;
+  static String getCameraScreen() => _cameraScreen;
+  static String getProfileScreen() => _profileScreen;
 
   static List<GetPage> routes = [
     //splash screen
@@ -59,5 +68,11 @@ class RouteHelper {
     GetPage(name: _contactScreen, page: () => const ContactScreen()),
     //message screen
     GetPage(name: _messageScreen, page: () => const MessageScreen()),
+    //phone screen
+    GetPage(name: _phoneScreen, page: () => const PhoneScreen()),
+    //camera screen
+    GetPage(name: _cameraScreen, page: () => const CameraScreen()),
+    //profile screen
+    GetPage(name: _profileScreen, page: () => const ProfileScreen()),
   ];
 }
