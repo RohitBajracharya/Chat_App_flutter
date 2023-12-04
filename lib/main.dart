6b063d1c785c410b8b0b246c100301cae690f0f7
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-12-04 19:05:04 +0545

    UI completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      defaultTransition: Transition.leftToRightWithFade,
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
       theme: ThemeData(
